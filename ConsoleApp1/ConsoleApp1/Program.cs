6f45a21b61b5aa8211d3ec578bf3d663bd9ebc01
Rodrigo Lara <ronfelara@gmail.com>
2018-02-26 23:59:20 -0300

    Commit_Xadrez_0.1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using tabuleiro;
+
+namespace jogo_xadrez
+{
+    class Program
+    {
+        static void Main(string[] args) { 
+            Posicao P;
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
+        }
+    }
+}
