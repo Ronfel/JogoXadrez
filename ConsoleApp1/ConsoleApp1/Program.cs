b60e5cdfb01277e982ecf33ea331bb2c2783b92d
Rodrigo Lara <ronfelara@gmail.com>
2018-03-08 00:22:46 -0300

    Criando Classe Posição Xadrez e melhorando impressão na Tela.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,14 +6,19 @@ namespace jogo_xadrez{
     class Program{
 
         static void Main(string[] args) {
-            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+            try {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Tela.imprimirTabuleiro(tab);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(5, 5));
 
+                Tela.imprimirTabuleiro(tab);
+            } catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
