baaca5b8294c17d5af16f48671f414c9ffa45e96
Rodrigo Lara <ronfelara@gmail.com>
2018-03-11 14:18:40 -0300

    Revert "Revert "Testes iniciais com os movimentos possiveis das pecas!""
    This reverts commit 73220decd604fdd38718d8fffb8f955fb58258bd.

--- ConsoleApp1/ConsoleApp1/tabuleiro/Peca.cs
+++ ConsoleApp1/ConsoleApp1/tabuleiro/Peca.cs
@@ -1,16 +1,23 @@
 ﻿namespace tabuleiro {
-    class Peca {
+    abstract class Peca {
         public Posicao posicao { get; set; }
         public Cor cor { get; set; }
         public int qteMovimentos { get; protected set; }
-        public Tabuleiro tabuleiro { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
 
-        public Peca(Tabuleiro tabuleiro, Cor cor) {
+        public Peca(Tabuleiro tab, Cor cor) {
             this.posicao = null;
-            this.tabuleiro = tabuleiro;
+            this.tab = tab;
             this.cor = cor;
             qteMovimentos = 0;
         }
+
+        public bool podeMover(Posicao pos) {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        public abstract bool[,] movimentosPossiveis();
 
         public void incrementarQteMovimentos() {
             qteMovimentos++;
